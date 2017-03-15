09a4540ddfd9f023634835232bc2f2bbd2b649b6
fs <fengshuo3@jd.com>
2017-03-15 18:12:38 +0800

    no message

--- MyFirst.podspec
+++ MyFirst.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'MyFirst'
-  s.version          = '0.3.0'
+  s.version          = '0.5.0'
   s.summary          = 'A short description of MyFirst.'
 
 # This description is used to generate tags and improve search results.
--- MyFirst/Classes/TestCCViewController.h
+++ MyFirst/Classes/TestCCViewController.h
@@ -1,5 +1,5 @@
 //
-//  TestViewController.h
+//  TestCCViewController.h
 //  Pods
 //
 //  Created by fs on 2017/3/15.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface TestViewController : UIViewController
+@interface TestCCViewController : UIViewController
 
 @end
