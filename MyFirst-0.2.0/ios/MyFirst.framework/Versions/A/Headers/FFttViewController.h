d7dfd8e2fa18622c50cd67ca9069525baa9b31a9
fs <fengshuo3@jd.com>
2017-03-15 18:01:01 +0800

    no message

---
+++ MyFirst-0.2.0/ios/MyFirst.framework/Versions/A/Headers/FFttViewController.h
@@ -0,0 +1,13 @@
+//
+//  FFttViewController.h
+//  MyFirst
+//
+//  Created by fs on 2017/3/15.
+//  Copyright © 2017年 fengshuo3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FFttViewController : UIViewController
+
+@end
---
+++ MyFirst-0.2.0/ios/MyFirst.framework/Versions/A/Headers/TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  Pods
+//
+//  Created by fs on 2017/3/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController : UIViewController
+
+@end
